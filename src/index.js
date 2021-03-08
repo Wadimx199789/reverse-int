37fbcd3cf94405317669882389e61d454db5fe7f
Wadim <martinkewichwadim@gmail.com>
2021-03-08 18:27:07 +0300

    fix: change index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ module.exports = function reverse (n) {
   if(n<0){
     n*=(-1);
 }
-
-  let arr=String(n).split('');
-  let newarr=[];
-  for(let i=arr.length-1;i>=0;i--){
-    newarr.push(arr[i]);
-  }
-  return newarr.join('');}
+return Number(String(n).split('').reverse().join(''));
+}
 
